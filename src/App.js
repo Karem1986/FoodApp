df59fcd14ed9afe2aba8eb41bd9b867d776d01c8
Karem <karem.ortiz@outlook.com>
2021-07-22 20:14:00 +0200

    fetching in console vegan recipes from api

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
+import React, { useEffect } from 'react'
 import './App.css';
 
 function App() {
+  //Random Recipes
+  const keyAPI = '309c92bbcb8d4dccb2872594735a1c3a'
+  // const randomRecipes = 'https://api.spoonacular.com/recipes/random'
+  // const pastaRecipes = 'https://api.spoonacular.com/recipes/complexSearch?query=pasta&apiKey=${keyAPI}'
+  // const vegetarianRecipes = 'https://api.spoonacular.com/recipes/complexSearch?query=vegetarian&apiKey=${keyAPI}'
+  // const veganGlutenFreeRecipes = 'https://api.spoonacular.com/recipes/complexSearch?query=vegan&number=15&intolerances=gluten&apiKey=${keyAPI}'
+useEffect(() => {
+   getRandomRecipes();
+}, [])
+
+const getRandomRecipes = async () => {
+  const response = await fetch(`https://api.spoonacular.com/recipes/complexSearch?query=vegan&number=10&intolerances=gluten&apiKey=${keyAPI}`)
+  const data = await response.json()
+  console.log('data', data)
+}
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <input type="text" />
+      <button type='submit'>Search</button>
     </div>
   );
 }
